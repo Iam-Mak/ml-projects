d99f4f64c6db3e5e531c1756c1b061afd28df47a
Amil <mohd.amilk09@gmail.com>
2025-12-18 11:43:31 +0000

    Model Eval. & Hyper. Tuining

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -42,7 +42,7 @@ class DataTransformation:
             cat_pipeline = Pipeline(
                 steps=[
                     ("imputer", SimpleImputer(strategy="most_frequent")),
-                    ("one_hot_encoder", OneHotEncoder(handle_unknown='ignore'))
+                    ("one_hot_encoder", OneHotEncoder(handle_unknown='ignore', sparse_output=False))
                 ]
             )
             
